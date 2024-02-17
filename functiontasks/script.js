f60791da1ff3bb82c917aab2052eba80913d75b6
Class04 <hassankhan5uy99@gmail.com>
2024-02-17 17:36:44 +0500

    function tasks added

---
+++ functiontasks/script.js
@@ -0,0 +1,38 @@
+
+// Question 01
+// let userName = prompt("Enter Your Name",);
+// function sayWelcome(userName) {
+//     console.log("Welcome " + userName + " 👋")
+// }
+// sayWelcome(userName);
+
+
+
+// Question 02
+function quotesGenerator() {
+    let quotes = [
+        "The ultimate aim of the ego is not to see something, but to be something.",
+        "Be aware of your own worth; use all of your power to achieve it.",
+        "The man who acquires knowledge but does not rid himself of ignorance is like one who drinks water and still remains thirsty.",
+        "Nations are born in the hearts of poets, they prosper and die in the hands of politicians.",
+        "The person who has no imagination has no wings.",
+        "Words, without power, is mere philosophy.",
+        "With faith, discipline and selfless devotion to duty, there is nothing worthwhile that you cannot achieve.",
+        "The best of artists has no conception that a marble block does not contain within itself.",
+        "The ultimate aim of the self is not to see something, but to be something.",
+        "Man is primarily governed by passion and instinct."
+    ];
+
+    let i = Math.floor(Math.random() * quotes.length);
+    console.log(quotes[i] + "🙏");
+}
+
+quotesGenerator();
+
+// Question 03
+// function randomNumGen(){
+//     let randomNum = Math.floor((Math.random() * 100)+1);
+//     console.log(randomNum);
+// }
+
+// randomNumGen()
